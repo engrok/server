5016aa72bc18b7726daa22cea45997c6b9f459a3
Sopiko Kurdadze <55315742+CSopiko@users.noreply.github.com>
2021-08-17 23:05:30 +0400

    fix first bug

--- src/app.js
+++ src/app.js
@@ -3,6 +3,6 @@ const wss = new WebSocket.Server({ port: '8080' });
 
 wss.on('connection', socket => {
     socket.on('message', message => {
-        socket.send(Message from client: ${message});
+        socket.send(`Message from client: ${message}`);
     });
-})
+});
