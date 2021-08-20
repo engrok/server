cc98ed914fb24a96781b03f583028d3d8082fb82
CSopiko <skurd18@freeuni.edu.ge>
2021-08-21 00:16:29 +0400

    retrieve local server data and send to third party client

--- src/app.js
+++ src/app.js
@@ -1,13 +1,23 @@
+const wssPort = '8080';
+const thirdPartyPort = 8888;
 const WebSocket = require('ws');
-const app = require('express')(); 
-const wss = new WebSocket.Server({ port: '8080' }); 
+const app = require('express')();
+const wss = new WebSocket.Server({ port: wssPort });
 let webSocket = null;
+let response = null;
 
 wss.on('connection', socket => {
     webSocket = socket;
+    webSocket.on('message', function incoming(data) {
+        if (response) {
+            console.log(data);
+            response.send(data.toString());
+        }
+        response = null;
+    });
 });
 
-app.listen(8888);
+app.listen(thirdPartyPort);
 
 app.get('/', function (req, res) {
     if (!webSocket) {
@@ -15,5 +25,5 @@ app.get('/', function (req, res) {
         return;
     }
     webSocket.send(JSON.stringify(req.headers));
-    res.send(JSON.stringify(req.headers));
+    response = res;
 });
