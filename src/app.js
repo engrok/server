83e8302c3e6139de23155daae6343fadcf2186f7
tamar-mepe <tdavi18@freeuni.edu.ge>
2021-08-22 14:49:32 +0400

    add id attribute

--- src/app.js
+++ src/app.js
@@ -2,19 +2,21 @@ const wssPort = '8080';
 const thirdPartyPort = 8888;
 const WebSocket = require('ws');
 const app = require('express')();
+const { v4: uuidv4 } = require('uuid');
 const wss = new WebSocket.Server({ port: wssPort });
 let webSocket = null;
-let response = null;
+let response = {};
 
 wss.on('connection', socket => {
     webSocket = socket;
     webSocket.on('message', function incoming(data) {
-        if (response) {
-            let passed = JSON.parse(data.toString());
-            response.set('content-type', passed.headers['content-type']);
-            response.send(passed.data);
+        let passed = JSON.parse(data);
+        let currResponse = response[passed.id];
+        if (currResponse) {
+            currResponse.set('content-type', passed.headers['content-type']);
+            currResponse.send(passed.data)
+            delete response[passed.id];
         }
-        response = null;
     });
 });
 
@@ -25,10 +27,12 @@ app.get('*', function (req, res) {
         res.send('Client not connected!');
         return;
     }
+    let id = uuidv4();
     let data = {
         url: req.originalUrl,
-        headers: req.headers
+        headers: req.headers,
+        id: id
     };
+    response[id] = res;
     webSocket.send(JSON.stringify(data));
-    response = res;
 });
